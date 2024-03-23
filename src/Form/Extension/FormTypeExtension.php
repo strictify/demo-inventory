ddc08f0db12c2a4c2b3739e082675efd2c30b692
Zeljko Mitic <mitke013@gmail.com>
2024-03-23 17:47:14 +0100

    Form collection improved

--- src/Form/Extension/FormTypeExtension.php
+++ src/Form/Extension/FormTypeExtension.php
@@ -6,40 +6,36 @@ namespace App\Form\Extension;
 
 use Override;
 use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use function count;
 
 /**
  * @extends AbstractTypeExtension<CollectionType>
  */
-class CollectionExtension extends AbstractTypeExtension
+class FormTypeExtension extends AbstractTypeExtension
 {
     #[Override]
     public static function getExtendedTypes(): iterable
     {
-        yield CollectionType::class;
+        yield FormType::class;
     }
 
     #[Override]
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'min' => 0,
-            'add_button_value' => 'Add',
-            'button_position' => 'after',
+            'collection_entry_class' =>  null,
         ]);
     }
 
     #[Override]
     public function buildView(FormView $view, FormInterface $form, array $options): void
     {
-        $nrOfChildren = count($form);
-        $view->vars['next_index'] = $nrOfChildren;
-        $view->vars['minimum'] = $options['min'] ?? 0;
-        $view->vars['add_button_value'] = $options['add_button_value'];
-        $view->vars['button_position'] = $options['button_position'];
+        $view->vars['collection_entry_class'] = $options['collection_entry_class'];
     }
 }
